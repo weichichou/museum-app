3c975d494a151e5932a6f23c69c22b79b9cc7a41
weichichou <weichi.cc@gmail.com>
2019-10-10 17:03:40 +0200

    Create elements with data from the form

--- pages/page_details.js
+++ pages/page_details.js
@@ -1,3 +1,22 @@
 function submitComment(){
-    console.log('Hello!')
+    const inputField = document.getElementById('name');
+    const name = inputField.value;
+  
+
+    const textArea = document.getElementById('msg');
+    const msg = textArea.value;
+    
+    
+    const comment = document.createElement('div');
+    const h4 = document.createElement('h4');
+    const p = document.createElement('p');
+
+    h4.innerHTML = `${name} says:`;
+    p.innerHTML = msg;
+    //comment.classList.add('comment');
+    comment.appendChild(h4);
+    comment.appendChild(p);
+
+   console.log(comment)
+
 }
