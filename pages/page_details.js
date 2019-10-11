8e964dac56cbcfe3c4e724d3e17204cf44432102
weichichou <weichi.cc@gmail.com>
2019-10-11 09:38:04 +0200

    Display user's input

--- pages/page_details.js
+++ pages/page_details.js
@@ -1,7 +1,7 @@
-function submitComment(){
+/* function submitComment(){
     const inputField = document.getElementById('name');
     const name = inputField.value;
-  
+    
 
     const textArea = document.getElementById('msg');
     const msg = textArea.value;
@@ -13,10 +13,46 @@ function submitComment(){
 
     h4.innerHTML = `${name} says:`;
     p.innerHTML = msg;
-    //comment.classList.add('comment');
+    comment.classList.add('comment');
     comment.appendChild(h4);
     comment.appendChild(p);
 
    console.log(comment)
 
-}
+
+   document.getElementById('commentContainer').appendChild(comment);
+
+} */
+
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById('form--userInput').addEventListener('submit',function(event){
+        
+        const inputField = document.getElementById('name');
+        const name = inputField.value;
+        
+    
+        const textArea = document.getElementById('msg');
+        const msg = textArea.value;
+        
+        
+        const comment = document.createElement('div');
+        const h4 = document.createElement('h4');
+        const p = document.createElement('p');
+    
+        h4.innerHTML = `${name} says:`;
+        p.innerHTML = msg;
+        comment.classList.add('comment');
+        comment.appendChild(h4);
+        comment.appendChild(p);
+    
+       console.log(comment)
+    
+    
+       document.getElementById('commentContainer').appendChild(comment);
+
+
+        event.preventDefault();
+    
+    })
+});
